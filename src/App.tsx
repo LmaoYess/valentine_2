815be8ca8752e2387e7504d08cc1e7388b5851dd
LmaoYess <148084436+LmaoYess@users.noreply.github.com>
2025-02-13 17:04:18 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,78 +27,36 @@ const App: React.FC = () => {
     }));
   };
 
-  return (
-    <div
-      className="container"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        textAlign: "center",
-        padding: "20px",
-      }}
-    >
-      {accepted ? (
-        <div
-          className="result"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <h1>Yeeeeeeey! I knew you'd say yes! ❤️❤️❤️❤️</h1>
-          <img
-            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHNneTF3a28wanJrazk5aHdkd2pqMHNpbXR4OWRvdHU2ODUxNW9wZyZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/KA8qBXr6zykcCvrNbi/giphy.gif"
-            alt="Happy Gif"
-            className="gif"
-            style={{ maxWidth: "100%", height: "auto" }}
-          />
+return (
+  <div className="valentine-container">
+    {accepted ? (
+      <div className="result">
+        <h1>Yeeeeeeey! I knew you'd say yes! ❤️❤️❤️❤️</h1>
+        <img
+          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHNneTF3a28wanJrazk5aHdkd2pqMHNpbXR4OWRvdHU2ODUxNW9wZyZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/KA8qBXr6zykcCvrNbi/giphy.gif"
+          alt="Happy Gif"
+          className="gif"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
+      </div>
+    ) : (
+      <div className="content">
+        <h1>Will you be my Valentine? ❤️</h1>
+        <img
+          src="https://media.giphy.com/media/ZDu3RRJZkp4rzbZ5d1/giphy.gif?cid=ecf05e47rbxpsp9q54od2w5sa7wbpf4xlzqyrrjw1wu5fajc&ep=v1_stickers_related&rid=giphy.gif&ct=s"
+          alt="Cute Gif"
+          className="gif"
+          style={{ maxWidth: "50%", height: "auto" }}
+        />
+        <div className="buttons">
+          <button id="yes" onClick={() => setAccepted(true)}>Yes ❤️</button>
+          <button id="no" style={noButtonStyle} onMouseOver={moveNoButton} onTouchStart={moveNoButton}>
+            No ? 🥺
+          </button>
         </div>
-      ) : (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <h1>Will you be my Valentine? ❤️</h1>
-          <img
-            src="https://media.giphy.com/media/ZDu3RRJZkp4rzbZ5d1/giphy.gif?cid=ecf05e47rbxpsp9q54od2w5sa7wbpf4xlzqyrrjw1wu5fajc&ep=v1_stickers_related&rid=giphy.gif&ct=s"
-            alt="Cute Gif"
-            className="gif"
-            style={{ maxWidth: "50%", height: "auto" }}
-          />
-          <div
-            className="buttons"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              gap: "20px",
-              marginTop: "20px",
-              position: "relative",
-              flexWrap: "nowrap",
-            }}
-          >
-            <button id="yes" onClick={() => setAccepted(true)}>
-              Yes ❤️
-            </button>
-            <button
-              id="no"
-              style={noButtonStyle}
-              onMouseOver={moveNoButton}
-              onTouchStart={moveNoButton} // Ensures it moves on touch devices
-            >
-              No ? 🥺
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
+      </div>
+    )}
+  </div>
+);
 
 export default App;
